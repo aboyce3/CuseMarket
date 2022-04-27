b3fcc7da4efb3f7d793187db5a633ea1bf528a61
Andrew Boyce <amboyce@syr.edu>
2022-04-27 18:36:10 -0400

    Added view controllers.

---
+++ CuseMarket/CuseMarket/ProfileViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileViewController.swift
+//  CuseMarket
+//
+//  Created by Andrew Boyce on 4/27/22.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
