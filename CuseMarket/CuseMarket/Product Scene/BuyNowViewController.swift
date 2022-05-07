f16d13cbbf87a0518c17bd4608b1bec857c2e7e7
Andrew Boyce <andyboyce30@gmail.com>
2022-05-06 23:08:16 -0400

    Buy Now Update and UI Update for it.

--- CuseMarket/CuseMarket/Product Scene/BuyNowViewController.swift
+++ CuseMarket/CuseMarket/Product Scene/BuyNowViewController.swift
@@ -6,18 +6,71 @@
 //
 
 import UIKit
-
+import SafariServices
 class BuyNowViewController: UIViewController {
     
+    @IBOutlet weak var fullName: UITextField!
+    @IBOutlet weak var address1: UITextField!
+    @IBOutlet weak var address2: UITextField!
+    @IBOutlet weak var city: UITextField!
+    @IBOutlet weak var zipCode: UITextField!
+    @IBOutlet weak var state: UITextField!
+    @IBOutlet weak var phoneNumber: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var cardNumber: UITextField!
+    @IBOutlet weak var monthYear: UITextField!
+    @IBOutlet weak var securityCode: UITextField!
     var productid: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
+
+    @IBAction func buyNowOnClick(_ sender: Any) {
+        if(fullName.text! == "" || address1.text == "" || city.text! == "" || zipCode.text! == "" || state.text! == "" || phoneNumber.text! == "" || email.text! == "" ||
+           cardNumber.text! == "" || monthYear.text! == "" || securityCode.text! == ""){
+            let dialogMessage = UIAlertController(title: "Confirm", message: "Please verify your information and try again!", preferredStyle: .alert)
+            
+            let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
+                return
+             })
+            
+            dialogMessage.addAction(ok)
+            self.present(dialogMessage, animated: true, completion: nil)
+        } else {
+            let dialogMessage = UIAlertController(title: "Confirm", message: "Purchase successful!", preferredStyle: .alert)
+            
+            let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
+                self.performSegue(withIdentifier: "returnSegue", sender: self)
+             })
+            
+            dialogMessage.addAction(ok)
+            self.present(dialogMessage, animated: true, completion: nil)
+        }
+    }
     
-    @IBAction func didTapBuyNow(_ sender: Any) {
+    @IBAction func paypalOnClick(_ sender: Any) {
+        let dialogMessage = UIAlertController(title: "Confirm", message: "Purchase successful!", preferredStyle: .alert)
+        
+        let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
+            self.performSegue(withIdentifier: "returnSegue", sender: self)
+         })
+        
+        dialogMessage.addAction(ok)
+        self.present(dialogMessage, animated: true, completion: nil)
+    }
+    
+    @IBAction func venmoOnClick(_ sender: Any) {
+        let dialogMessage = UIAlertController(title: "Confirm", message: "Purchase successful!", preferredStyle: .alert)
+        
+        let ok = UIAlertAction(title: "OK", style: .default, handler: { (action) -> Void in
+            self.performSegue(withIdentifier: "returnSegue", sender: self)
+         })
+        
+        dialogMessage.addAction(ok)
+        self.present(dialogMessage, animated: true, completion: nil)
     }
     
 }
+
